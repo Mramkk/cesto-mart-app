0f0303b52b0e3e101da170f74b37907b9e4ea62a
Mramkk <rramkumarbedia@gmail.com>
2024-01-13 16:54:42 +0530

    evening

--- lib/config/urls.dart
+++ lib/config/urls.dart
@@ -1,2 +1,2 @@
-const baseUrl = "http://192.168.1.5/cesto-mart/";
-// const baseUrl = "https://cestomart.com/app/";
+// const baseUrl = "http://192.168.1.5/cesto-mart/";
+const baseUrl = "https://cestomart.com/survey-app/";
